d98738db5069536a0e0a0920ef6fd0880ef21ac4
Daniel Mawalla <mawalladaniel2018@gmail.com>
2024-09-05 15:53:18 +0300

    Update connectDB.js

--- KilimoKipya_Phase_2/backend/config/connectDB.js
+++ KilimoKipya_Phase_2/backend/config/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+console.log(process.env.MONGO_DB_URL);
 mongoose.connect(process.env.MONGO_DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
