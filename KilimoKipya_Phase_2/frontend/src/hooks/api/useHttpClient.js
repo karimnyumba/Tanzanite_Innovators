46471531745715b008ec8a6a8db8c031b5b1b7a6
Nampellah <60631465+Francisnampellah@users.noreply.github.com>
2025-05-20 11:52:03 +0300

    Update useHttpClient.js

--- KilimoKipya_Phase_2/frontend/src/hooks/api/useHttpClient.js
+++ KilimoKipya_Phase_2/frontend/src/hooks/api/useHttpClient.js
@@ -4,7 +4,9 @@ import { notify } from "../../utils/helper/notification";
 import { notifyType } from "../../utils/helper/notificationType";
 import { useCookies } from "react-cookie";
 
-axios.defaults.baseURL = "https://kilimokipya.up.railway.app/";
+axios.defaults.baseURL = "https://kilimo.bluetech.software/";
+// https://kilimo.bluetech.software/
+// https://kilimokipya.up.railway.app/
 // https://tanzanite-innovators.onrender.com/
 const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
